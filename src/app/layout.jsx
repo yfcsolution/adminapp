8e5ef62960e4ec1c8e65057a54ecbcb1c451ec1e
Ijaz Wakel <ijazwakeel.dev@example.com>
2025-03-19 15:33:58 +0500

    chnages addede

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,20 @@
+"use client"; // Mark this component as a client component
+
 import "./globals.css";
 import { AuthProvider } from "./common/auth-context";
 import { ToastProvider } from "@/components/ToastProvider";
+import { useEffect } from "react"; // Import useEffect
+import axios from "axios"; // Import axios
+
 export default function RootLayout({ children }) {
   useEffect(() => {
     const intervalId = setInterval(() => {
       // Make the GET request to the cron URL using axios without logging
       axios.get("https://waserver.pro/cron/execute-webhook").catch((error) => {
         // Handle error silently if necessary
+        console.error("Error in cron job request:", error);
       });
-    }, 60000);
+    }, 60000); // 60 seconds
 
     // Cleanup function to clear the interval when the component unmounts
     return () => clearInterval(intervalId);
@@ -22,6 +28,7 @@ export default function RootLayout({ children }) {
           "/api/messages/whatsapp/webhooks/sync"
         );
         // You can handle the response here if needed
+        console.log("Data synced successfully:", response.data);
       } catch (error) {
         console.error("Error syncing data:", error);
       }
@@ -36,12 +43,12 @@ export default function RootLayout({ children }) {
     // Cleanup on component unmount
     return () => clearInterval(intervalId);
   }, []);
+
   return (
     <html lang="en">
       <body>
         <AuthProvider>
           <ToastProvider />
-
           {children}
         </AuthProvider>
       </body>
