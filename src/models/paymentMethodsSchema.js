5674ccc04d4c424c154f6bd03393e0eebee7e1b7
Ijaz Wakel <ijazwakeel.dev@example.com>
2025-04-20 02:58:06 +0500

    chnages added

--- src/models/paymentMethodsSchema.js
+++ src/models/paymentMethodsSchema.js
@@ -5,13 +5,11 @@ const paymentMethodsSchema = new mongoose.Schema(
   {
     MethodId: {
       type: Number,
-      unique: true,
       default: 1, // Default is 1
     },
     MethodName: {
       type: String,
       required: true, // Make MethodName required
-      unique: true,
     },
     Available: {
       type: Boolean,
@@ -23,26 +21,6 @@ const paymentMethodsSchema = new mongoose.Schema(
     minimize: false, // Prevent mongoose from removing empty objects
   }
 );
-
-// Pre-save hook to handle auto-increment logic for 'MethodId'
-paymentMethodsSchema.pre("save", async function (next) {
-  if (this.isNew) {
-    try {
-      // Fetch the last document based on MethodId, sorted in descending order
-      const lastMethod = await mongoose
-        .model("PaymentMethods")
-        .findOne()
-        .sort({ MethodId: -1 });
-
-      // Increment MethodId, or start from 1 if no document exists
-      this.MethodId = lastMethod ? lastMethod.MethodId + 1 : 1;
-    } catch (error) {
-      console.error("Error in auto-increment logic: ", error);
-      return next(error); // Pass the error to the next middleware
-    }
-  }
-  next(); // Continue with the save operation
-});
 
 // Export the model, ensuring it's only created once
 export default mongoose.models.PaymentMethods ||
